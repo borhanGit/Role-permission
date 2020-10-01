813f01408fd3b12aaf77c4f84a1de7db28ec6367
mdborhan <mdborhan410@gmail.com>
2020-10-01 13:06:05 +0600

    first commit

---
+++ app/Http/Controllers/Backend/RoleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleController extends Controller
+{
+
+    public function index()
+    {
+
+        $roles= Role::all();
+        return view('backend.pages.role.index',[
+            'roles'=>$roles
+        ]);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
